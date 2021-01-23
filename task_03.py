a3c198a96e8104a92f3df67bb438d5e68128c501
evogul <evogul@mail.ru>
2021-01-23 09:41:02 +0300

    корректировка для пул реквеста

--- task_03.py
+++ task_03.py
@@ -1,11 +1,18 @@
 '''
 Реализовать функцию my_func(), которая принимает три позиционных аргумента,
-и возвращает сумму наибольших двух аргументов
+и возвращает сумму наибольших двух аргументов.
+исправлено
 '''
 def my_func(num1, num2, num3):
-    return max(num1, num2, num3)
+    min_num = min(num1, num2, num3)
+    if min_num == num1:
+        return num2 + num3
+    elif min_num == num2:
+        return num1 + num3
+    else:
+        return num + num2
 
-num1 = int(input('input first number: '))
-num2 = int(input('input second number: '))
-num3 = int(input('input third number: '))
-print(f'maximum is: {my_func(num1, num2, num3)}')
+num1 = int(input('введите первое число: '))
+num2 = int(input('введите второе число: '))
+num3 = int(input('введите третье: '))
+print(f'сумма двух наибольших чисел : {my_func(num1, num2, num3)}')
